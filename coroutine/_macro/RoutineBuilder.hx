5637841ff7287b93bb8486c28eb6e6b2402efd57
dpomier <dpomier@spiele-palast.de>
2020-03-02 18:33:25 +0100

    improve null parsing

--- coroutine/_macro/RoutineBuilder.hx
+++ coroutine/_macro/RoutineBuilder.hx
@@ -7,44 +7,30 @@ import haxe.macro.ExprTools;
 class RoutineBuilder {
 
     static function init () {
-
         yield.parser.Parser.parseOnImport("coroutine.Routine");
         yield.parser.Parser.parseOnImport("coroutine.Routine.RoutineInstruction");
         yield.parser.Parser.onYield(onYield);
-
     }
 
-    static function onYield (e:Expr, t:Null<ComplexType>):Null<Expr> {
-
+    static function onYield (e:Expr, t:Null<ComplexType>):Null<Expr>
         return switch t {
-            case macro:coroutine.Routine.RoutineInstruction:
-                buildInstruction(e, true);
+            case (macro:coroutine.Routine.RoutineInstruction):
+                transformInstruction(e);
+                null;
             case _:
                 null;
         }
-    }
 
-    static function buildInstruction (e:Expr, typed:Bool):Null<Expr> {
-
-        return switch e {
-
-            case null:
-
-                null;
-            
+    static function transformInstruction (e:Expr):Void
+        switch e {
             case (macro ($w)):
-
-                buildInstruction(w, typed);
-
-            case macro null if( typed ):
-
-                macro (coroutine.Routine.RoutineInstruction.WaitNextFrame : coroutine.Routine.RoutineInstruction);
-
-            case _:
-
-                null;
+                transformInstruction(w);
+            case { expr: expr, pos: pos }:
+                e.expr = (macro switch (${{ expr: expr, pos: pos }}) {
+                    case null: (coroutine.Routine.RoutineInstruction.WaitNextFrame : coroutine.Routine.RoutineInstruction);
+                    case v: (v : coroutine.Routine.RoutineInstruction);
+                }).expr;
         }
-    }
 
 }
 #end
